9ebd75ba572d28558ce16e0fb4111958aec4db9b
Noah_Yang <ykd1217@gmail.com>
2018-11-18 12:11:21 +0900

    geolocation

--- packages/web/src/components/Map/Map.js
+++ packages/web/src/components/Map/Map.js
@@ -6,13 +6,30 @@ export class MapComponent extends Component {
   mapRef = createRef();
 
   componentDidMount() {
+    this.createMap();
+    this.setCurrentPosition();
+  }
+
+  createMap = () => {
     const options = {
       center: new window.daum.maps.LatLng(33.450701, 126.570667),
-      level: 3,
+      level: 4,
     };
 
     this.map = new window.daum.maps.Map(this.mapRef.current, options);
-  }
+  };
+
+  setCurrentPosition = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const locPosition = new window.daum.maps.LatLng(lat, lon);
+
+        this.map.setCenter(locPosition);
+      });
+    }
+  };
 
   render() {
     return <div className={this.props.className} ref={this.mapRef} />;
